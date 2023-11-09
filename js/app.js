4194db97fa577f5fc3c31ccf7d51934542e50bbe
JohnnyBackus <john.p.backus@gmail.com>
2023-11-09 15:23:47 -0800

    lab time over, got as far as we could

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@
 function AppState() {
   this.allProducts = [];
 }
+function Product(name, fileExtension = 'jpg') {
+  this.name = name;
+  this.source = `assets/${name}.${fileExtension}`;
+  this.timesClicked = 0;
+  this.timesShown = 0;
+}
 
 AppState.prototype.instantiateProducts = function () {
 
@@ -17,23 +23,42 @@ AppState.prototype.instantiateProducts = function () {
   }
 
 }
-
+// // TODO: Fill in this instance method to save product data to local storage
 AppState.prototype.saveToLocalStorage = function () {
-  // TODO: Fill in this instance method to save product data to local storage
-}
+  if (this.allProducts.length > 0) {
 
+  const products = JSON.stringify(this.allProducts);
+  console.log(products);
+  localStorage.setItem('appState', products);
+  }
+}
+// console.log('read', AppState)
+
+// // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
 AppState.prototype.loadItems = function () {
+  let productsString = localStorage.getItem('appState');
+  if (productsString) {
+    let parsedProducts = JSON.parse(productsString);
 
-  // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
-
-  this.instantiateProducts();
-
+    
+    for (let i = 0; i < parsedProducts.length; i++) {
+      this.allProducts.push(new Product(parsedProducts[i].name));
+    }
+    console.log(productsString);
+    console.log(parsedProducts);
+    // for (let i = 0; i < products.length; i++) {
+    // if (products[i] == allProducts[i]); {
+    //   this.allProducts.push
+    // }
+  }
+  else {
+    this.instantiateProducts();
+  }
 }
 
 
-function Product(name, fileExtension = 'jpg') {
-  this.name = name;
-  this.source = `assets/${name}.${fileExtension}`;
-  this.timesClicked = 0;
-  this.timesShown = 0;
-}
+
+let myAppState = new AppState ();
+myAppState.instantiateProducts ();
+myAppState.loadItems ();
+myAppState.saveToLocalStorage ();
